4ed93253f08a090e6e7aa479c5c655265a153949
Bilal Baş <bilal.bas@gmx.de>
2023-08-24 08:22:47 +0300

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Bilal Baş on 24.08.23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
